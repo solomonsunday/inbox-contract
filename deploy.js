779ccf54cece720d0627a0d2fa38372dbf7af1ba
solomon sunday <schinoyerem007@gmail.com>
2023-12-08 11:19:52 +0100

    update: deploy inbox contract to infura

--- deploy.js
+++ deploy.js
@@ -1,5 +1,29 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-const { Web3 } = require('web3');
-//updated web3 and hdwallet-provider imports added for convenience
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const { Web3 } = require("web3");
+const { interface, bytecode } = require("./compile");
+require("dotenv").config();
 
 // deploy code will go here
+
+const provider = new HDWalletProvider(
+  `${process.env.mnemonic}`,
+  "https://goerli.infura.io/v3/e21e775d1b7143e591bd8b41e5f09a72"
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  try {
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts[0], "account");
+
+    const result = await new web3.eth.Contract(JSON.parse(interface))
+      .deploy({ data: bytecode, arguments: ["Hi there"] })
+      .send({ gas: 1000000, from: accounts[0] });
+
+    console.log("contract deploy to", result.options.address);
+    provider.engine.stop();
+  } catch (error) {
+    console.log(error);
+  }
+};
+deploy();
